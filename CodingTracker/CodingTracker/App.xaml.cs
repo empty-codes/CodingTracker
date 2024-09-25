22a78d38dde05f9635755ac367cb522eb1974173
empty-codes <92234322+empty-codes@users.noreply.github.com>
2024-09-25 11:30:05 +0100

    Added package references, Added config settings, Redefined Application Structure

--- CodingTracker/CodingTracker/App.xaml.cs
+++ CodingTracker/CodingTracker/App.xaml.cs
@@ -1,10 +1,19 @@
-﻿namespace CodingTracker
+﻿using Microsoft.VisualBasic;
+
+namespace CodingTracker
 {
     public partial class App : Application
     {
+        public static string? ConnectionString { get; private set; }
+        public static string? DateFormat { get; private set; }
+        public static string? DatabasePath { get; private set; } 
         public App()
         {
             InitializeComponent();
+
+            ConnectionString = "Data Source=coding_tracker.db;Provider=Microsoft.Data.SQLite";
+            DateFormat = "yyyy-MM-dd HH:mm";
+            DatabasePath = "coding_tracker.db";
 
             MainPage = new AppShell();
         }
