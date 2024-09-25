ec800c5317dfbb853aa5551b8f792174200ff220
empty-codes <92234322+empty-codes@users.noreply.github.com>
2024-09-25 18:48:36 +0100

    Created views and viewmodels for viewing all sessions

---
+++ CodingTracker/CodingTracker/ViewModels/CodingSessionViewModel.cs
@@ -0,0 +1,76 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+
+namespace CodingTracker.ViewModels;
+
+internal class CodingSessionViewModel : ObservableObject
+{
+    private Models.CodingSession codingSession;
+
+    public int Id => codingSession.Id;
+    public DateTime StartTime
+    {
+        get => codingSession.StartTime;
+        set
+        {
+            if (codingSession.StartTime != value)
+            {
+                codingSession.StartTime = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public DateTime EndTime
+    {
+        get => codingSession.EndTime;
+        set
+        {
+            if (codingSession.EndTime != value)
+            {
+                codingSession.EndTime = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public TimeSpan Duration
+    {
+        get => codingSession.Duration;
+        set
+        {
+            if (codingSession.Duration != value)
+            {
+                codingSession.Duration = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public ICommand UpdateCommand { get; private set; }
+    public ICommand DeleteCommand { get; private set; }
+
+    public CodingSessionViewModel()
+    {
+        codingSession = new Models.CodingSession();
+        UpdateCommand = new RelayCommand(Update);
+        DeleteCommand = new RelayCommand(Delete);
+    }
+
+    public CodingSessionViewModel(Models.CodingSession session)
+    {
+        codingSession = session;
+        UpdateCommand = new RelayCommand(Update);
+        DeleteCommand = new RelayCommand(Delete);
+    }
+
+    private void Update()
+    {
+        codingSession.CalculateDuration();
+        codingSession.UpdateSession(codingSession);
+    }
+
+    private void Delete()
+    {
+        codingSession.UpdateSession(codingSession);
+    }
+}
+
---
+++ CodingTracker/CodingTracker/ViewModels/ViewAllSessionsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using CodingTracker.Models;
+
+namespace CodingTracker.ViewModels;
+
+internal class ViewAllSessionsViewModel
+{
+    public ObservableCollection<ViewModels.CodingSessionViewModel> AllSessions { get; }
+    public ICommand NewCommand { get; }
+    public ICommand SelectSessionCommand { get; }
+
+    public ViewAllSessionsViewModel()
+    {
+        AllSessions = new ObservableCollection<ViewModels.CodingSessionViewModel>(Models.CodingSession.ViewAllSessions().Select(n => new CodingSessionViewModel(n)));
+        NewCommand = new RelayCommand(NewSession);
+        SelectSessionCommand = new RelayCommand<ViewModels.CodingSessionViewModel>(SelectSession);
+    }
+
+    private void NewSession()
+    {
+        Shell.Current.GoToAsync(nameof(Views.CodingSessionPage));
+    }
+
+    private void SelectSession(ViewModels.CodingSessionViewModel session)
+    {
+        if(session != null)
+        {
+            Shell.Current.GoToAsync($"{nameof(Views.CodingSessionPage)}?id={session.Id}");
+        }
+    }
+}
+
